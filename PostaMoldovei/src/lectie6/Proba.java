7cace6a259bb39373ee85b73cf75bbbd233027cd
Gerakles <bogdania567@gmail.com>
2017-11-13 18:23:33 +0200

    first commit

--- PostaMoldovei/src/lectie6/Proba.java
+++ PostaMoldovei/src/lectie6/Proba.java
@@ -4,22 +4,33 @@ import java.util.Scanner;
 
 public class Proba {
     private static String login;
+    private static String login1;
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.println("Write login: ");
+        Scanner sc1 = new Scanner(System.in);
+        System.out.println("Ваше имя");
         login = sc.nextLine();
+        login1 = sc1.nextLine();
 
-        String testing = ".1066";
-        int indexsc = testing.indexOf(".");
-         if(indexsc == 1066) {
-             System.out.println("Very good");
+         if(login.equals("Богдан")) {
+             System.out.println("Я вас привествую мой господин!");
+             System.out.println("Желаю вам всего наилучшего!");
+
+             if(login1.equals("Спасибо")) {
+                 System.out.println("Я рад что угил вам");
+             }  else {
+                 System.out.println("Fail");
+             }
          }  else {
              System.out.println("Fail");
          }
 
+
+
          }
 
-         private static String testing;
+
     }
+
 
