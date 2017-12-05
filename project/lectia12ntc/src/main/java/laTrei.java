979c0e5721e6380f13ce1060140e92e566d23b1f
Gerakles <bogdania567@gmail.com>
2017-12-06 00:09:55 +0200

    Yakim present

--- project/lectia12ntc/src/main/java/laTrei.java
+++ project/lectia12ntc/src/main/java/laTrei.java
@@ -1,6 +1,6 @@
 public class laTrei {
     public static void main(String[] args) {
-        for (int i = 3; i <=100 ; i+=3) {
+        for (int i = 3; i <=100 ; i+=3) {//toate nr se imparte la 3
                 System.out.println(i+ ": дельтся на 3 без остатка!");
         }
     }
