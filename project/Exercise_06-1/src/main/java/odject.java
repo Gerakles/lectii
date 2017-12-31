c63511f2c817e2b852c28de6957da642e63c96eb
Gerakles <bogdania567@gmail.com>
2017-12-31 19:24:02 +0200

    my 4 commit

---
+++ project/Exercise_06-1/src/main/java/odject.java
@@ -0,0 +1,18 @@
+public class odject {
+    public static void main(String[] args) {
+        int ID;
+        String descr;
+        double price;
+        int quantity;
+    }
+
+//    public static void main(String[] args) {
+//        String name = "Yakimchiuk Bogdan";
+//        int custID = 1066;
+//        String adress = "Pietrarilor 14/3 ap 23";
+//        int orderNum;
+//        int age = 19;
+//
+//        System.out.println( "Customer : " + name );
+//    }
+}
--- project/lectia6n1/src/main/java/Item.java
+++
@@ -1,6 +0,0 @@
-public class Item {
-    public int itemID;
-    public String desc;
-    public double price;
-    public int quantity;
-}
