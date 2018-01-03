ed87652bc0924601ebadbd72900d0edcb988c9b5
Gerakles <bogdania567@gmail.com>
2018-01-03 02:09:33 +0200

    my 5 commit

--- project/Exercise_04-2/src/main/java/Shop.java
+++ project/Exercise_04-2/src/main/java/Shop.java
@@ -12,3 +12,4 @@ public class Shop {
         System.out.println("Total cost with tax is: "+total);
     }
 }
+
---
+++ project/Starter/src/main/java/number2/Bytte.java
@@ -0,0 +1,8 @@
+package number2;
+
+public class Bytte {
+    public static void main(String[] args) {
+        byte a = 7;
+        System.out.println("Every "+a+" day says:\n Soli Deo Gloria");
+    }
+}
