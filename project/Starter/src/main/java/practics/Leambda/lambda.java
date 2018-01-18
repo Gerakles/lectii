bbef29e80167158f9e40a925b615f7749f67df93
Gerakles <bogdania567@gmail.com>
2018-01-18 23:57:07 +0200

    my 29 commit

--- project/Starter/src/main/java/practics/Leambda/lambda.java
+++ project/Starter/src/main/java/practics/Leambda/lambda.java
@@ -5,12 +5,13 @@ import java.util.ArrayList;
 public class lambda {
     public static void main(String[] args) {
         Incrementer incrementer = x -> x+1;
-        System.out.println(incrementer.increment(1));
+        System.out.println(incrementer.increment(1));// Lambda где x +1
 
         ArrayList<Integer> ints = new ArrayList <>();
         ints.add( 1 );
         ints.add( 2 );
         ints.add( 3 );
-        ints.forEach( integer -> System.out.println(integer+1) );
+        ints.forEach( integer -> System.out.println(integer+1) );// с помошью lambda плюсуем все +1
+        ints.forEach(System.out::println);//здесь просто печатаем ArrayList
     }
 }
