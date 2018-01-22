f2c9ec9189a9c9c5b46573fb369377d508c9aa84
Gerakles <bogdania567@gmail.com>
2018-01-22 19:42:30 +0200

    my 34 commit

---
+++ project/CodeWars/src/main/java/Kata.java
@@ -0,0 +1,22 @@
+
+
+public class Kata {
+    public static int sum(int[] numbers) {
+        int sum = 0;
+        int sum2 = 0;
+        int min = numbers[0];
+        int max = numbers[0];
+
+        for (int num : numbers) {
+            sum += num;
+            if (num < min) {
+                min = num;
+            }
+            if (num > max) {
+                max = num;
+            }
+            sum2 = sum - max - min;
+        }
+        return sum2;
+    }
+}
