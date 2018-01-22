740d7b1a033543ee28659397a063095e8fb2a124
Gerakles <bogdania567@gmail.com>
2018-01-22 22:46:51 +0200

    my 36 commit

---
+++ project/CodeWars/src/main/java/WhichAreIn.java
@@ -0,0 +1,8 @@
+import java.util.Arrays;
+
+public class WhichAreIn {
+
+    public static String[] lastArray(String[] array1, String[] array2) {
+        return Arrays.stream(array1).filter(str -> Arrays.stream(array2).anyMatch( s -> s.contains(str))).distinct().sorted().toArray(String[]::new);
+    }
+}
