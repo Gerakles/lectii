06d933568248b51bece3fe07ec6d6ff5b2c8db08
Gerakles <bogdania567@gmail.com>
2018-02-01 22:35:04 +0200

    Yakim 49

--- project/CodeWars/src/main/java/Dinglemouse.java
+++
@@ -1,8 +0,0 @@
-public class Dinglemouse {
-
-    public static int[] humanYearsCatYearsDogYears(final int humanYears) {
-        return new int[]{humanYears, humanYears==1?15 :humanYears==2 ? 24: 24+4*(humanYears-2)
-                , humanYears==1?15 :humanYears==2 ? 24: 24+5*(humanYears-2)};
-    }
-
-}
--- project/CodeWars/src/main/java/WhichAreIn.java
+++
@@ -1,8 +0,0 @@
-import java.util.Arrays;
-
-public class WhichAreIn {
-
-    public static String[] lastArray(String[] array1, String[] array2) {
-        return Arrays.stream(array1).filter(str -> Arrays.stream(array2).anyMatch( s -> s.contains(str))).distinct().sorted().toArray(String[]::new);
-    }
-}
