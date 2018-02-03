c069e7d2922c0bbde8bb1df8088734ddc6d0cab7
Gerakles <bogdania567@gmail.com>
2018-02-03 21:49:15 +0200

    Yakim 54

---
+++ project/auth/src/main/java/model/Admin.java
@@ -0,0 +1,32 @@
+package model;
+
+public class Admin {
+    String logins;
+    String passwords;
+
+    public Admin(String logins, String passwords) {
+        this.logins = logins;
+        this.passwords = passwords;
+    }
+
+    public String getLogins() {
+        return logins;
+    }
+
+    public void setLogins(String logins) {
+        this.logins = logins;
+    }
+
+    public String getPasswords() {
+        return passwords;
+    }
+
+    public void setPasswords(String passwords) {
+        this.passwords = passwords;
+    }
+
+    @Override
+    public String toString() {
+        return "Administrotor {" + logins + " " + passwords + "}";
+    }
+}
---
+++ project/auth/src/main/java/service/UserList.java
@@ -0,0 +1,19 @@
+package service;
+
+import model.Admin;
+import model.User;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class UserList {
+    public static List <User> users;
+    public static List <Admin> admins;
+
+    static {
+        users = new ArrayList <>();
+        admins.add( new Admin( "Gerakles", "pass" ) );
+        users.add( new User( "test", "test" ) );
+        users.add( new User( "Holop", "pas" ) );
+    }
+}
