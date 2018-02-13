f5329212c22940212f945a66566d943f0bd37176
Gerakles <bogdania567@gmail.com>
2018-02-13 21:47:28 +0200

    Yakim 484

--- project/CodeWars/src/main/java/kyu5/Solution.java
+++ project/CodeWars/src/main/java/kyu5/Solution.java
@@ -6,7 +6,7 @@ import java.util.stream.Collectors;
 class Solution {
 
     static String toCamelCase(String s) {
-        String[] local = s.split( "[-|_]" );
+        String[] local = s.split( "[ ]" );
         return local[0] + Arrays
                 .stream( local )
                 .skip( 1 ).map( y -> y.substring( 0, 1 ).
--- project/demos/src/test/java/HappyBirthdayTest.java
+++ project/demos/src/test/java/HappyBirthdayTest.java
@@ -1,2 +1,14 @@
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runners.JUnit4;
+
 public class HappyBirthdayTest {
+    @Test
+    public void test() {
+        Assert.assertEquals( 16,HappyBirthday.yakim( "Iachimciuc Bogdan" ) );
+        Assert.assertEquals( 12,HappyBirthday.yakim( "Blanari Denis" ) );
+        Assert.assertEquals( 7,HappyBirthday.yakim( "Vitalie" ) );
+        Assert.assertEquals( 6,HappyBirthday.yakim( "Andrei" ) );
+        Assert.assertEquals( 0,HappyBirthday.yakim( " " ) );
+    }
 }
