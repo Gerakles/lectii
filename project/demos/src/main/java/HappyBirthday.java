fc532dfcf48bb09cebd2e715a55b088afeb0e8f6
Gerakles <bogdania567@gmail.com>
2018-02-14 07:45:43 +0200

    Yakim 483

--- project/demos/src/main/java/HappyBirthday.java
+++ project/demos/src/main/java/HappyBirthday.java
@@ -1,10 +1,10 @@
 public class HappyBirthday {
     public static int yakim(String words) {
-        int count = 0;
+        int a = 0;
         for (int i = 0; i <words.length() ; i++) {
             if (Character.isLetter( words.charAt( i ) ))
-                count++;
+                a++;
         }
-        return count;
+        return a;
     }
 }
