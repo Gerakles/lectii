f5f894c670e20f69a2fc93996e458bab33a5f706
Gerakles <bogdania567@gmail.com>
2018-02-15 03:12:50 +0200

    Yakim 482

---
+++ project/CodeWars/src/main/java/kyu8/EvenOrOdd.java
@@ -0,0 +1,13 @@
+package kyu8;
+
+public class EvenOrOdd {
+    public static String even_or_odd(int number) {
+        String yalim1 = "Even";
+        String yalim2 = "Odd";
+
+        if (number%2==0)
+            return yalim1;
+        else
+            return yalim2;
+    }
+}
