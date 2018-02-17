77152f533800a51df05447100a63ca90d2d43abb
Gerakles <bogdania567@gmail.com>
2018-02-17 23:32:39 +0200

    Yakim 481

--- project/CodeWars/src/main/java/kyu7/Kata.java
+++ project/CodeWars/src/main/java/kyu7/Kata.java
@@ -2,17 +2,16 @@ package kyu7;
 
 class Kata {
     public static String getMiddle(String word) {
-        String a = word;
         int position;
         int lenght;
-        if (a.length() % 2 == 1){
-            position = a.length() / 2;
+        if (word.length() % 2 == 1){
+            position = word.length() / 2;
             lenght = 1;
         }else {
-            position = a.length()/2-1;
+            position = word.length()/2-1;
             lenght=2;
         }
-        String answer = a.substring( position,position+lenght );
+        String answer = word.substring( position,position+lenght );
         return answer;
     }
 }
