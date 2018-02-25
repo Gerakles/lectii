1e933faf9f53091251c106b9be2baec2dc9b4ce6
Gerakles <bogdania567@gmail.com>
2018-02-25 21:34:26 +0200

    Yakim 473

--- project/auth/src/main/java/service/RoleList.java
+++ project/auth/src/main/java/service/RoleList.java
@@ -6,24 +6,26 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class RoleList {
-    public static List<Role> rols;
+    public static List <Role> rols;
 
     static {
-        rols = new ArrayList<>();
-        rols.add( new Role( "user" ,1, ResurcesList.findUserResourse() ));
-        rols.add( new Role( "admin", 2,ResurcesList.resours ) );
+        rols = new ArrayList <>();
+        rols.add( new Role( "user", 1, ResurcesList.findUserResourse() ) );
+        rols.add( new Role( "admin", 2, ResurcesList.resours ) );
     }
-    public static Role findByName (String name) {
-        for (int i = 0; i <rols.size() ; i++) {
+
+    public static Role findByName(String name) {
+        for (int i = 0; i < rols.size(); i++) {
             if (rols.get( i ).getName().equals( name ))
                 return rols.get( i );
 
         }
         return null;
     }
-    public static Role findById (int id) {
-        for (int i = 0; i <rols.size() ; i++) {
-            if (rols.get( i ).getId() == id )
+
+    public static Role findById(int id) {
+        for (int i = 0; i < rols.size(); i++) {
+            if (rols.get( i ).getId() == id)
                 return rols.get( i );
 
         }
