53f774762a58d22a0b3f885144b4f41021fed7d3
Gerakles <bogdania567@gmail.com>
2018-02-25 21:36:59 +0200

    Yakim 473

--- project/Mesto_peremen/src/main/java/Start.java
+++ project/Mesto_peremen/src/main/java/Start.java
@@ -7,15 +7,15 @@ public class Start {
         Authentification mp = new Authentification();
         Scanner sc = new Scanner( System.in );
         boolean exit = false;
-        while (!exit&&sc.hasNext()) {
+        while (!exit && sc.hasNext()) {
             String comander = sc.nextLine();
             String[] place = comander.split( " " );
             switch (place[0]) {
                 case "login":
-                    if (mp.login( place[1],place[2] ))
-                        System.out.println(mp.getCarentUser());
+                    if (mp.login( place[1], place[2] ))
+                        System.out.println( mp.getCarentUser() );
                     else
-                        System.out.println("Good buy");
+                        System.out.println( "Good buy" );
             }
         }
     }
