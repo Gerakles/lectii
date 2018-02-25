6552915c86d9202d733b73dde47f40588568fbc9
Gerakles <bogdania567@gmail.com>
2018-02-25 21:35:02 +0200

    Yakim 473

--- project/auth/src/main/java/service/UserList.java
+++ project/auth/src/main/java/service/UserList.java
@@ -11,24 +11,24 @@ public class UserList {
     static final String url = "jdbc:sqlite:D://lectii/src/db/auth.db";
     public static UserList service = new UserList();
 
-    public List<User> findAll() {
+    public List <User> findAll() {
         String sql = "SELECT id, name, password,id_role FROM user";
-        List<User> users = new ArrayList <>(  );
+        List <User> users = new ArrayList <>();
 
         try (Connection conn = this.connect();
              Statement stmt = conn.createStatement();
-             ResultSet rs = stmt.executeQuery(sql)) {
+             ResultSet rs = stmt.executeQuery( sql )) {
 
             // loop through the result set
             while (rs.next()) {
-                int id =rs.getInt("id");
-                String userName = rs.getString("name");
-                String password = rs.getString("password");
-                int idRole = rs.getInt("id_role");
-                users.add( new User( id,userName,password, idRole ) );
+                int id = rs.getInt( "id" );
+                String userName = rs.getString( "name" );
+                String password = rs.getString( "password" );
+                int idRole = rs.getInt( "id_role" );
+                users.add( new User( id, userName, password, idRole ) );
             }
         } catch (SQLException e) {
-            System.out.println(e.getMessage());
+            System.out.println( e.getMessage() );
         }
         return users;
     }
@@ -37,9 +37,9 @@ public class UserList {
         // SQLite connection string
         Connection conn = null;
         try {
-            conn = DriverManager.getConnection(url);
+            conn = DriverManager.getConnection( url );
         } catch (SQLException e) {
-            System.out.println(e.getMessage());
+            System.out.println( e.getMessage() );
         }
         return conn;
     }
@@ -48,24 +48,25 @@ public class UserList {
         String sql = "INSERT INTO user(name,password,id_role) VALUES(?,?,2)";
 
         try (Connection conn = this.connect();
-             PreparedStatement pstmt = conn.prepareStatement(sql)) {
-            pstmt.setString(1, name);
-            pstmt.setString(2, passowrd);
+             PreparedStatement pstmt = conn.prepareStatement( sql )) {
+            pstmt.setString( 1, name );
+            pstmt.setString( 2, passowrd );
             pstmt.executeUpdate();
         } catch (SQLException e) {
-            System.out.println(e.getMessage());
+            System.out.println( e.getMessage() );
         }
         return null;
     }
-    public void deleteUser (String name) {
+
+    public void deleteUser(String name) {
         String sql = "DELETE FROM user Where name=?";
 
         try (Connection conn = this.connect();
-             PreparedStatement pstmt = conn.prepareStatement(sql)) {
-            pstmt.setString(1, name);
+             PreparedStatement pstmt = conn.prepareStatement( sql )) {
+            pstmt.setString( 1, name );
             pstmt.executeUpdate();
         } catch (SQLException e) {
-            System.out.println(e.getMessage());
+            System.out.println( e.getMessage() );
         }
     }
 
