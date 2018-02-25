a435d2b7c60df8d43908905d7c647e40fdd9a899
Gerakles <bogdania567@gmail.com>
2018-02-25 16:26:38 +0200

    Yakim 473

--- project/Mesto_peremen/src/main/java/service/Authentification.java
+++ project/Mesto_peremen/src/main/java/service/Authentification.java
@@ -1,4 +1,14 @@
 package service;
 
+import body.User;
+
 public class Authentification {
+    public User carentUser;
+
+    public boolean login(String login, String password) {
+        for (User user: UserList.service.findAll() ) {
+            
+        }
+        return false;
+    }
 }
--- project/Mesto_peremen/src/main/java/service/UserList.java
+++ project/Mesto_peremen/src/main/java/service/UserList.java
@@ -1,4 +1,18 @@
 package service;
 
+import body.User;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class UserList {
+    // SQLite connection string
+    static final String url = "jdbc:sqlite:D://lectii/src/db/mesto_peremen.db";
+    public static UserList service =new UserList();
+
+    public List<User> findAll() {
+        String sql = "SELECT id, name, password,id_role FROM user";
+        List <User> users = new ArrayList <>();
+        return users;
+    }
 }
