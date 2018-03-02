f2235abd71b0851fa50b90e2ee58cac87bf3397e
Gerakles <bogdania567@gmail.com>
2018-03-02 22:54:32 +0200

    Yakim 468

---
+++ project/Essential/src/main/java/L2/Animal/Cat.java
@@ -0,0 +1,19 @@
+package L2.Animal;
+
+public class Cat {
+    private String name;
+    private int age;
+
+    public Cat(String n, int a) {
+        name = n;
+        age = a;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+}
