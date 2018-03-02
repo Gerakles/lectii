5a3dde326aef9e49f78176eb3a938b564adf4d3f
Gerakles <bogdania567@gmail.com>
2018-03-02 22:58:15 +0200

    Yakim 468

--- project/Essential/src/main/java/L2/Main.java
+++ project/Essential/src/main/java/L2/Main.java
@@ -6,6 +6,6 @@ public class Main {
     public static void main(String[] args) {
         Cat cat = new Cat( "Bob", 7 );
 
-        System.out.println();
+        System.out.println(cat.getName()+" "+cat.getAge());
     }
 }
