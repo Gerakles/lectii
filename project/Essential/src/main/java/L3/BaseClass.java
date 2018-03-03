0d85525d3dd9dbf3918e04d99e4496bba934dcfa
Gerakles <bogdania567@gmail.com>
2018-03-03 21:05:01 +0200

    Yakim 567

---
+++ project/Essential/src/main/java/L3/BaseClass.java
@@ -0,0 +1,7 @@
+package L3;
+
+public class BaseClass {
+    public String publicField = "BaseClass.publicField";
+    private String privateField = "BaseClass.privateField";
+    protected String protectedField = "BaseClass.protectedField";
+}
