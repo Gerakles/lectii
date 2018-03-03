b1b452de47f30f9a813bc35eb061c04bf296e168
Gerakles <bogdania567@gmail.com>
2018-03-03 23:01:41 +0200

    Yakim 567

--- project/Essential/src/main/java/L3/Main2.java
+++ project/Essential/src/main/java/L3/Main2.java
@@ -3,7 +3,8 @@ package L3;
 public class Main2 {
     public static void main(String[] args) {
         derivedClass instance = new derivedClass();
+
         System.out.println(instance.publicField);
-        System.out.println(instance.protectedField);
+        instance.show();
     }
 }
